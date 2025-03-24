3c47444dde77f084e55fa32064592bf524711a61
fitterfly-gyanit <gyaneshwar.thokal@fitterfly.in>
2025-03-24 18:26:48 +0530

    changes

--- angular-tut/src/app/app.component.ts
+++ angular-tut/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterAppComponent } from "./counter-project/counter-project.component";
 
@@ -10,6 +10,51 @@ import { CounterAppComponent } from "./counter-project/counter-project.component
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tut';
-  name = "Gyani"
+  count=signal(0);
+  x=20;
+
+  userName=signal('anil');
+
+
+  displayHeading=false;
+
+  constructor(){
+    effect(()=>{
+      // console.log(this.x);
+      //console.log(this.count());
+      //console.log(this.userName());
+
+      if(this.count()==2){
+        this.displayHeading=true;
+        setTimeout(()=>{
+          this.displayHeading=false;
+        },2000)
+      }else{
+        this.displayHeading=false;
+      }
+    })
+  }
+
+  updateSignal(val:string){
+    //this.x= 30;
+    //this.count=signal(100);
+    //this.count.set(this.count()+1);
+    //this.x= this.x+1;
+
+    if(val=="incr"){
+      this.count.set(this.count()+1);
+    }else{
+      if(this.count()==0){
+        this.count.set(0);
+      }else{
+        this.count.set(this.count()-1);
+      }
+    }
+  }
+
+
+  toggleValue(){
+    this.count.set(this.count()+1);
+    //this.displayHeading=!this.displayHeading
+  }
 }
--- angular-tut/src/app/counter-project/counter-project.component.ts
+++ angular-tut/src/app/counter-project/counter-project.component.ts
@@ -36,4 +36,44 @@ export class CounterAppComponent {
   }
 
 
+  // handleEvent(event:any){
+  //   console.log('Function Called', event);
+  //   console.log('Event Type', event.type);
+  //   console.log('Event Class Name', (event.target as Element).className);
+  // }
+
+  handleEvent(event:any){
+    console.log('Function Called', event.type);
+    // console.log('Value', (event.target as HTMLInputElement).value);
+    //console.log('Function foucs', event.type);
+    //console.log('Function blur', event.type);
+  } 
+
+
+  name =""
+  displayName =""
+
+  email=""
+
+  getName(event:Event){
+    this.name = (event.target as HTMLInputElement).value;
+  }
+  
+  showName(){
+    this.displayName = this.name;
+  }
+
+  setName(){
+    this.name = "Sam";
+  }
+
+  getEmail(val:string){
+    console.log(val);
+    this.email = val;
+  }
+
+  setEmail(){
+    this.email = "Default@test.com";
+  }
+
 }
